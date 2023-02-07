879bb8305c35d7f4351168f4f966a6ca739a5d2a
Ffranciscolascoaga <123431236+Ffranciscolascoaga@users.noreply.github.com>
2023-02-07 14:18:45 -0300

    Update Idea Olascoaga.sql

--- Idea Olascoaga.sql
+++ Idea Olascoaga.sql
@@ -5,7 +5,7 @@ Create DATABASE if not exists Empresa_de_limpieza;
 Use Empresa_de_Limpieza;
 
 #Creacion de TABLAS#
-Drop Table `Clientes`;
+Drop Table if exists `Clientes`;
 
 CREATE TABLE IF NOT EXISTS `Clientes` (
     id_Cliente INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY ,
@@ -15,10 +15,12 @@ CREATE TABLE IF NOT EXISTS `Clientes` (
     Correo varchar(50), 
     telefono VARCHAR(15),
     direccion VARCHAR(100) NOT NULL,
-    Empresa varchar(50) not null
-);
+    Empresa varchar(50) not null,
+    Metodo_pago varchar(18));
 
-Drop table `Proveedores`;
+select * from Clientes;
+
+Drop table if exists `Proveedores`;
 
 	CREATE TABLE IF NOT EXISTS `proveedores` (
     id_proveedor INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
@@ -30,7 +32,7 @@ Drop table `Proveedores`;
     direccion VARCHAR(100) NOT NULL,
     costo numeric (10,2)
 );
-    
+    select * from Proveedores;
     
     Drop table if exists `Empleados`
     
@@ -42,10 +44,14 @@ CREATE TABLE IF NOT EXISTS `Empleados` (
     genero CHAR(1),
     estado_civil VARCHAR(15),
     fecha_ingreso DATE NOT NULL,
+    fecha_nac date not null,
     telefono VARCHAR(15),
     correo VARCHAR(50),
     direccion VARCHAR(100) NOT NULL
 );
+    
+    select * from Empleados;
+    
     
     Drop table if exists `Empresa`
     
@@ -57,12 +63,16 @@ CREATE TABLE IF NOT EXISTS `Empresa` (
     correo VARCHAR(50),
     direccion VARCHAR(100) NOT NULL
 );
+  select * from Empresa;
+  
    drop table if exists  `productos`
     CREATE TABLE IF NOT EXISTS `productos` (
     id_producto INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    Proveedor int,
     nombres_producto VARCHAR(50) NOT NULL,
     precio numeric(10,2)
 );
+   select * from productos;
    
       drop table if exists  `facturas`
     CREATE TABLE IF NOT EXISTS `facturas` (
@@ -70,11 +80,15 @@ CREATE TABLE IF NOT EXISTS `Empresa` (
     id_Proveedor VARCHAR(50) NOT NULL,
     precio numeric(10,2)
 );
-      drop table if exists  `recibo sueldo`;
-    CREATE TABLE IF NOT EXISTS `recibo sueldo` (
+   select * from facturas;
+   
+   
+      drop table if exists  `recibo_sueldo`;
+    CREATE TABLE IF NOT EXISTS `recibo_sueldo` (
     id_Recibo_Sueldo INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     id_Empleado VARCHAR(50) NOT NULL);
     
+    select * from recibo_sueldo;
 
 drop table if exists `datos`;
 CREATE TABLE IF NOT EXISTS `datos` (
@@ -82,12 +96,15 @@ CREATE TABLE IF NOT EXISTS `datos` (
     correo VARCHAR(50),
     direccion VARCHAR(100) NOT NULL
 );
+
+select * from datos;
     
      drop table if exists  `Antiguedad`;
     CREATE TABLE IF NOT EXISTS `Antiguedad` (
     id_Empleado INT NOT NULL UNIQUE AUTO_INCREMENT,
    Antiguedad  VARCHAR(50) NOT NULL);
       
+      select * from Antiguedad;
  
  drop table if exists `servicio`;
     CREATE TABLE IF NOT EXISTS `servicio` (
@@ -95,6 +112,8 @@ CREATE TABLE IF NOT EXISTS `datos` (
     Oficina VARCHAR(50),
     fin_de_obra VARCHAR(50),
     casa VARCHAR(50));
+    
+    select * from servicio;
     
       Drop table if exists `FormaDe_Pago`;
     
@@ -104,20 +123,25 @@ Create Table if not Exists `FormaDe_Pago` (
     Transferencia_bancaria  varchar(2), 
     Efectivo  varchar(2)
     );
+    select * from formaDe_pago;
     
       Drop table if exists  `Movimientos`;
     
     Create Table if not Exists `Movimientos` (
     Movimientos varchar(100) not null);
     
+    select * from Movimientos;
+    
       Drop table if exists `Seguros`;
     
     Create Table if not Exists `Seguros` (
     ID_Seguros varchar (50) not null primary key, 
     Empleado int not null, 
-    ICliente Varchar (50) not null, 
+    clientes Varchar (50) not null, 
     Aseguradora varchar (50) not null
     );
+    
+    select * from Seguros;
     
       Drop table if exists `Equipacion`;
     
@@ -128,6 +152,7 @@ Create Table if not Exists `FormaDe_Pago` (
    Camionetas varchar (100),
    Existencias varchar (50)
     );
+    select * from equipacion;
     
       Drop table if exists  `Contrato`;
     
@@ -135,3 +160,5 @@ Create Table if not Exists `FormaDe_Pago` (
     ID_Contrato varchar (100) not null unique primary key, 
     id_Clientes int not null
     );
+    
+    select * from Contrato;
