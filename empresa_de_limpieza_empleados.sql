560badb53f0c7129334128194b93e0e0efcc4547
Ffranciscolascoaga <123431236+Ffranciscolascoaga@users.noreply.github.com>
2023-03-06 12:02:26 -0300

    Add files via upload
    Las tablas que se tomaron para realizar un backup incluyendo solamente datos son las siguientes:
    Antiguedad
    Clientes
    Contrato
    datos
    Empleados
    Empresa
    Equipacion
    Facturas
    Forma de pago
    Movimientos
    Productos
    Proveedores 
    Recibo de Sueldo
    Seguros 
    Servicio

---
+++ empresa_de_limpieza_empleados.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: empresa_de_limpieza
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (1,'41999083','Cristian','Busquet','M','soltero','2020-12-12','1980-08-10','2314552314','cristianbusquet@gmail.com','almafuerte 123'),(2,'20888765','Santiago','Sosa','M','soltero','2020-12-12','1988-10-10','2314552314','santisosa@gmail.com','boer 1234'),(3,'31456789','Mercedes','urrutia','F','casada','2020-12-12','1990-12-12','2350324500','meruruu@gmail.com','chiclana 50'),(4,'45987654','Catalina','Rodriguez','F','soltera','2020-12-12','2000-03-01','2314678912','cataro@gmail.com','paso 124'),(5,'30456235','Nicolas','Ochiato','M','soltero','2020-12-12','1999-10-12','2350657834','nicoochiato@gmail.com','alcorta 654');
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-06 11:56:34
